560d6092551e062b26836ac5890d9f458d442e83
Stephan Pieterse <stephan@apollolms.co.za>
2018-12-23 00:44:55 -0500

    Permission update
    Setting script to run as root in container,
    as the app fails to start if you don't have
    permissions set up correctly previously
    
    Signed-off-by: Stephan Pieterse <stephan@apollolms.co.za>

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ NAME=drumgen
 docker build -t $NAME:latest .
 docker stop $NAME
 docker rm $NAME
-docker run -u 1000 --restart=always --cpu-shares=1280 --memory=768mb --memory-swap=1024mb -d \
+docker run --restart=always --cpu-shares=1280 --memory=768mb --memory-swap=1024mb -d \
    -p 127.0.0.1:9051:5061 \
    -v `pwd`/cache/:/opt/app/cache/ \
    -v `pwd`/tmpgen/:/opt/app/tmpgen/ \
